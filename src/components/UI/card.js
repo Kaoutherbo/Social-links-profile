82e495767e0efa983b260bbd350f21484a3dc527
kaouther <kaoutherboutheldja@gmail.com>
2024-03-24 02:22:39 +0100

    deploy the website

--- src/components/UI/card.js
+++ src/components/UI/card.js
@@ -4,7 +4,7 @@ import Links from './links'
 
 export default function card({image, name, location, description, github, frontendMentor, linkdin, x, insta}) {
   return (
-    <div className='flex items-center flex-col bg-dark-grey p-8 rounded-lg text-white w-350'>
+    <div className='flex items-center flex-col bg-dark-grey p-8 rounded-lg text-white w-500'>
         <img src={image} alt="profile" className="rounded-full w-24 h-24 mb-6"/>
         <h1 className='text-3xl font-normal mb-0.5'>{name}</h1>
         <h3 className='text-primary text-sm mb-4'>{location}</h3>
