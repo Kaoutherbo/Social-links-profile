e58e62f6baaa34ae76d3128af874c94648070ddf
kaouther <kaoutherboutheldja@gmail.com>
2024-03-24 02:33:28 +0100

    deploy the website

--- src/components/UI/card.js
+++ src/components/UI/card.js
@@ -6,9 +6,9 @@ export default function card({image, name, location, description, github, fronte
   return (
     <div className='flex items-center flex-col bg-dark-grey p-8 rounded-lg text-white w-500'>
         <img src={image} alt="profile" className="rounded-full w-24 h-24 mb-6"/>
-        <h1 className='text-3xl font-normal mb-0.5'>{name}</h1>
+        <h1 className='text-2xl font-meduim mb-0.5'>{name}</h1>
         <h3 className='text-primary text-sm mb-4'>{location}</h3>
-        <p className='font-normal text-sm  mb-3 text-gray-300'>" {description} "</p>
+        <p className='font-normal text-sm  mb-5 text-gray-300'>" {description} "</p>
         <Links  
         github={github} 
         frontendMentor={frontendMentor} 
--- src/components/UI/links.js
+++ src/components/UI/links.js
@@ -3,11 +3,11 @@ import '../../styles/output.css'
 export default function links({github, frontendMentor, linkdin, x, insta}) {
   return (
     <div  className="">
-        <button className='text-gray-300 rounded-lg h-10 w-full bg-grey hover:bg-primary hover:text-grey duration-300 mb-3'><a href={github}>Github</a></button>
-        <button className='text-gray-300 rounded-lg h-10 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-3'><a href={frontendMentor}>Frontend Mentor</a></button>
-        <button className='text-gray-300 rounded-lg h-10 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-3'><a href={linkdin}>LinkedIn</a></button>
-        <button className='text-gray-300 rounded-lg h-10 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-3'><a href={x}>X</a></button>
-        <button className='text-gray-300 rounded-lg h-10 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-3'><a href={insta}>Instagram</a></button>
+        <button className='text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary hover:text-grey duration-300 mb-4'><a href={github}>Github</a></button>
+        <button className='text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-4'><a href={frontendMentor}>Frontend Mentor</a></button>
+        <button className='text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-4'><a href={linkdin}>LinkedIn</a></button>
+        <button className='text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-4'><a href={x}>X</a></button>
+        <button className='text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary  hover:text-grey duration-300 mb-4'><a href={insta}>Instagram</a></button>
     </div>
   )
 }
