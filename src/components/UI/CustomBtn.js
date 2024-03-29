9bde129de93d6bf99514f3b3c211abca2dab3e6e
kaouther <kaoutherboutheldja@gmail.com>
2024-03-29 18:57:15 +0100

    add some changes

--- src/components/UI/CustomBtn.js
+++ src/components/UI/CustomBtn.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function ({link, linkName}) {
+export default function CustomBtn({ link, linkName}) {
   return (
-    <button className='text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary hover:text-grey duration-300 mb-4'><a href={link}>{linkName}</a></button>
-  )
+    <button className="text-gray-300 rounded-lg h-11 w-full bg-grey hover:bg-primary hover:text-grey duration-300 mb-4">
+      <a href={link}>{linkName}</a>
+    </button>
+  );
 }
